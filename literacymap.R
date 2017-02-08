3652671ecdacdd64d0be9ae78fd123af32bc9a45
taramatthews <matthews.tara.m@gmail.com>
2017-02-08 12:29:25 -0500

    Geocoding Things
    Geocoded all the stuff.

--- literacymap.R
+++ literacymap.R
@@ -1,9 +1,15 @@
 # Geocoding Early Literacy Resources in Pittsburgh
 
+library(ggmap)
 
+load.lit <- read.csv("./literacy-map-resources.csv")
 
-load.snow_angel.1$Address_Street <- paste0(load.snow_angel.1$Address_Street, ", Pittsburgh, PA ", load.snow_angel.1$ARC_ZIP)
-2
-XY <- geocode(load.snow_angel.1$Address_Street)
-3
-load.snow_angel <- cbind(load.snow_angel.1, XY) 
+load.lit$fullAddress <- paste0(load.lit$Street.Address, ", Pittsburgh, PA ", load.lit$Zip.Code)
+
+load.lit$XY <- geocode(load.lit$fullAddress)
+
+wa# load.snow_angel.1$Address_Street <- paste0(load.snow_angel.1$Address_Street,", Pittsburgh, PA ", load.snow_angel.1$ARC_ZIP)
+# 2
+# XY <- geocode(load.snow_angel.1$Address_Street)
+# 3
+# load.snow_angel <- cbind(load.snow_angel.1, XY) 
